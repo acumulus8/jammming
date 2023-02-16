e1d11e5037de5c5e2ebfb8e83932cc5f0e3e8918
Tim <timwilburn8@gmail.com>
2023-02-16 01:44:22 -0500

    fix: removed useless constructor

--- src/Components/LoginContent/LoginContent.js
+++ src/Components/LoginContent/LoginContent.js
@@ -2,10 +2,6 @@ import React, { Component } from "react";
 import "./login-content.css";
 
 class LoginContent extends Component {
-	constructor(props) {
-		super(props);
-	}
-
 	render() {
 		return (
 			<div className="login-content">
