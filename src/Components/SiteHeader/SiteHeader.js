b1f064ee1009348f44e85264655e28af56db3ae7
Tim <timwilburn8@gmail.com>
2023-02-16 01:41:29 -0500

    fix: removed useless constructor

--- src/Components/SiteHeader/SiteHeader.js
+++ src/Components/SiteHeader/SiteHeader.js
@@ -2,10 +2,6 @@ import React, { Component } from "react";
 import "./site-header.css";
 
 class SiteHeader extends Component {
-	constructor(props) {
-		super(props);
-	}
-
 	render() {
 		return (
 			<div className="site-header">
