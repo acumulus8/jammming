792905f6d9a7574ef347df5d782d9eb8924bcc8e
Tim <timwilburn8@gmail.com>
2023-02-16 01:43:14 -0500

    fix: removed unused import

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 import "./Playlist.css";
 import TrackList from "../TrackList/TrackList";
 import TrashCan from "../../image_assets/trash-can-icon.png";
